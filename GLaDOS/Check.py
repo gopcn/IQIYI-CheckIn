8bd21d4175d23dc2b75b638820ef9447f48f6aa3
神经蛙 <121377609+gopcn@users.noreply.github.com>
2023-08-08 12:42:34 +0800

    Add files via upload

---
+++ GLaDOS/Check.py
@@ -0,0 +1,38 @@
+import json
+import requests
+from fake_useragent import UserAgent
+
+URL = "https://glados.rocks/api/user/checkin"
+URL2 = "https://glados.rocks/api/user/status"
+REFERER = 'https://glados.rocks/console/checkin'
+ORIGIN = "https://glados.rocks"
+UA = UserAgent()
+
+def CheckIn(cookie):
+    useragent = get_random_user_agent()
+    payload = {
+        'token': 'glados.one'
+    }
+    headers = {
+        'cookie': cookie,
+        'referer': REFERER,
+        'origin': ORIGIN,
+        'user-agent': useragent,
+        'content-type': 'application/json;charset=UTF-8'
+    }
+
+    checkin = requests.post(URL, headers=headers, json=payload)
+    state = requests.get(URL2, headers=headers)
+
+    mess = checkin.json()['message']
+    time = state.json()['data']['leftDays']
+    days = time.split('.')[0]
+    msg = f'checkin: {checkin.status_code} | state: {state.status_code}\n{mess}\n剩余天数：{days}天'
+
+    checkin.close()
+    state.close()
+    return f'{mess}，剩余{days}天', msg
+
+def get_random_user_agent():
+    random_user_agent = UA.random
+    return random_user_agent
---
+++ GLaDOS/logger.py
@@ -0,0 +1,28 @@
+import logging
+from pytz import timezone
+from datetime import datetime
+
+# 修复时区
+def Shanghai(sec, what):
+    tz = timezone('Asia/Shanghai')
+    timenow = datetime.now(tz)
+    return timenow.timetuple()
+
+logging.Formatter.converter = Shanghai
+
+# 创建logger对象
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)  # log等级总开关
+
+formatter = logging.Formatter("%(asctime)s [%(levelname)s]:  %(message)s")
+
+# 控制台handler
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO) # log等级的开关
+stream_handler.setFormatter(formatter)
+
+# 移除之前的handler
+logger.handlers.clear()
+
+# 添加新的handler
+logger.addHandler(stream_handler)
